454800be004d628889daf5e500067a7a66f5358b
Ricardo Kevin Gomes <ricardokevin@redelucass.local>
2023-10-16 14:41:09 -0300

    Padronizando banco de dados

---
+++ IntegracaoMilvusQlik/Migrations/20231016111335_initial1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace IntegracaoMilvusQlik.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "lista",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    categoria_primaria = table.Column<string>(type: "text", nullable: true),
+                    categoria_secundaria = table.Column<string>(type: "text", nullable: true),
+                    total_avaliacao = table.Column<string>(type: "text", nullable: true),
+                    descricao_avaliacao = table.Column<string>(type: "text", nullable: true),
+                    tecnico = table.Column<string>(type: "text", nullable: true),
+                    mesa_trabalho = table.Column<string>(type: "text", nullable: true),
+                    data_solucao = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    servico_realizado = table.Column<string>(type: "text", nullable: true),
+                    data_agendamento = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    data_resposta = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    equipamento_retirado = table.Column<string>(type: "text", nullable: true),
+                    material_utilizado = table.Column<string>(type: "text", nullable: true),
+                    setor = table.Column<string>(type: "text", nullable: true),
+                    tipo_ticket = table.Column<string>(type: "text", nullable: true),
+                    prioridade = table.Column<string>(type: "text", nullable: true),
+                    codigo = table.Column<int>(type: "integer", nullable: true),
+                    cliente = table.Column<string>(type: "text", nullable: true),
+                    cliente_token = table.Column<string>(type: "text", nullable: true),
+                    assunto = table.Column<string>(type: "text", nullable: true),
+                    descricao = table.Column<string>(type: "text", nullable: true),
+                    contato = table.Column<string>(type: "text", nullable: true),
+                    email_conferencia = table.Column<string>(type: "text", nullable: true),
+                    telefone = table.Column<string>(type: "text", nullable: true),
+                    data_criacao = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    data_modificacao = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    total_horas = table.Column<string>(type: "text", nullable: true),
+                    origem = table.Column<string>(type: "text", nullable: true),
+                    status = table.Column<string>(type: "text", nullable: true),
+                    impacto = table.Column<string>(type: "text", nullable: true),
+                    urgencia = table.Column<string>(type: "text", nullable: true),
+                    status_sla_resposta = table.Column<string>(type: "text", nullable: true),
+                    status_sla_solucao = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_lista", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "lista");
+        }
+    }
+}
--- IntegracaoMilvusQlik/Models/Lista.cs
+++ IntegracaoMilvusQlik/Models/Lista.cs
@@ -21,94 +21,123 @@ namespace IntegracaoMilvusQlik.Models
         public string? CategoriaSecundaria { get; set; }
 
         [JsonProperty("total_avaliacao")]
+        [Column("total_avaliacao")]
         public string? TotalAvaliacao { get; set; }
 
         [JsonProperty("descricao_avaliacao")]
+        [Column("descricao_avaliacao")]
         public string? DescricaoAvaliacao { get; set; }
 
         [JsonProperty("tecnico")]
+        [Column("tecnico")]
         public string? Tecnico { get; set; }
 
         [JsonProperty("mesa_trabalho")]
+        [Column("mesa_trabalho")]
         public string? MesaTrabalho { get; set; }
 
         [JsonProperty("data_solucao")]
+        [Column("data_solucao")]
         public DateTime? DataSolucao { get; set; }
 
         [JsonProperty("servico_realizado")]
+        [Column("servico_realizado")]
         public string? ServicoRealizado { get; set; }
 
         [JsonProperty("data_agendamento")]
+        [Column("data_agendamento")]
         public DateTime? DataAgendamento { get; set; }
 
         [JsonProperty("data_resposta")]
+        [Column("data_resposta")]
         public DateTime? DataResposta { get; set; }
 
         [JsonProperty("equipamento_retirado")]
+        [Column("equipamento_retirado")]
         public string? EquipamentoRetirado { get; set; }
 
         [JsonProperty("material_utilizado")]
+        [Column("material_utilizado")]
         public string? MaterialUtilizado { get; set; }
 
         [JsonProperty("setor")]
+        [Column("setor")]
         public string? Setor { get; set; }
 
         [JsonProperty("tipo_ticket")]
+        [Column("tipo_ticket")]
         public string? TipoTicket { get; set; }
 
         [JsonProperty("prioridade")]
+        [Column("prioridade")]
         public string? Prioridade { get; set; }
 
         [JsonProperty("codigo")]
+        [Column("codigo")]
         public int? Codigo { get; set; }
 
         [JsonProperty("cliente")]
+        [Column("cliente")]
         public string? Cliente { get; set; }
 
         [JsonProperty("cliente_token")]
+        [Column("cliente_token")]
         public string? ClienteToken { get; set; }
 
         [JsonProperty("assunto")]
+        [Column("assunto")]
         public string? Assunto { get; set; }
 
         [JsonProperty("descricao")]
+        [Column("descricao")]
         public string? Descricao { get; set; }
 
         [JsonProperty("contato")]
+        [Column("contato")]
         public string? Contato { get; set; }
 
         [JsonProperty("email_conferencia")]
+        [Column("email_conferencia")]
         public string? EmailConferencia { get; set; }
 
         [JsonProperty("telefone")]
+        [Column("telefone")]
         public string? Telefone { get; set; } 
 
         [JsonProperty("data_criacao")]
+        [Column("data_criacao")]
         public DateTime? DataCriacao { get; set; } 
 
         [JsonProperty("data_modificacao")]
+        [Column("data_modificacao")]
         public DateTime? DataModificacao { get; set; }
 
         [JsonProperty("total_horas")]
+        [Column("total_horas")]
         public string? TotalHoras { get; set; }
 
         [JsonProperty("origem")]
+        [Column("origem")]
         public string? Origem { get; set; }
 
         [JsonProperty("status")]
+        [Column("status")]
         public string? Status { get; set; }
 
         [JsonProperty("impacto")]
+        [Column("impacto")]
         public string? Impacto { get; set; }
 
         [JsonProperty("urgencia")]
+        [Column("urgencia")]
         public string? Urgencia { get; set; }
 
         [JsonProperty("status_sla_resposta")]
+        [Column("status_sla_resposta")]
         public string? StatusSlaResposta { get; set; }
 
         [JsonProperty("status_sla_solucao")]
+        [Column("status_sla_solucao")]
         public string? StatusSlaSolucao { get; set; }
     }
 }
-
